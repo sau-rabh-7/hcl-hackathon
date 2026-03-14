22fe303a5982fdeeef67914f502bd7a4a07140ad
ccu1507 <gauravsoniccu15@gmail.com>
2026-03-14 09:35:53 +0530

    Add Gaurav Soni to project contributors

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 // Middleware
 //hi its gaurav'
 //we are in hcl hackathon
+//lets start
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
